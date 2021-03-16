4611d1f648ed2add65148caccd2e056dc05c35dd
cahos1159 <tsicrelorge@gmail.com>
2021-03-16 13:56:34 +0100

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+<div>
+  <form>
+    <label for="in">Input numbers separated by space </label>
+    <input type="text" v-model="currentInput" name="in"/>
+  </form>
+  <button v-on:click="closerToZero()">Launch</button>
+  <p>{{ res }}</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+   data() {
+    return {
+      res: '',
+      currentInput: ''
+    };
+  },
+   methods: {
+    closerToZero() {
+      const arr = this.currentInput.split(" ").filter(elem => (parseInt(elem) == elem));
+      if (arr.length === 0) {
+         this.res = 0;
+      }
+      else{
+        this.res = arr[0];
+        arr.forEach(element => {
+          if ( Math.abs(element) < Math.abs(this.res) || Number(element) === Math.abs(this.res) ) {
+            this.res = element
+          }
+        });
+      }
+    }
+  }
+};
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
